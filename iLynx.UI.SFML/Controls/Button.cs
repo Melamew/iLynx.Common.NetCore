f07ed039a2c6173b7c0b6f3bf05913d7989ceaba
Melanie Hjorth <melanie@hjorth.com>
2018-05-30 20:19:32 +0100

    Saving work.

--- iLynx.UI.SFML/Controls/Button.cs
+++ iLynx.UI.SFML/Controls/Button.cs
@@ -6,7 +6,7 @@ using iLynx.UI.Controls;
 
 namespace iLynx.UI.SFML.Controls
 {
-    public class Button : IButton
+    public class Button// : IButton
     {
         public uint Width { get; set; }
         public uint Height { get; set; }
@@ -14,7 +14,7 @@ namespace iLynx.UI.SFML.Controls
         public Color Background { get; set; }
         public Color Foreground { get; set; }
         public IControl Content { get; set; }
-        public event EventHandler<MouseButtonEventArgs> Clicked;
-        public event EventHandler<MouseEventArgs> MouseOver;
+        //public event EventHandler<MouseButtonEventArgs> Clicked;
+        //public event EventHandler<MouseEventArgs> MouseOver;
     }
 }
