302b78292ddb92e0ad0ec90a74b873b9c27f1a59
Melanie Hjorth <melanie@hjorth.com>
2018-05-31 01:38:32 +0100

    A bit of work on bindings, still figuring out how these will work.

--- iLynx.Common/IValueBindingSource.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace iLynx.Common
-{
-    public interface IValueBindingSource
-    {
-        void AddValueChangedHandler<TValue>(string valueName, ValueChangedHandler<TValue> handler);
-        void RemoveValueChangedHandler<TValue>(string valueName, ValueChangedHandler<TValue> handler);
-    }
-}
--- iLynx.UI.TestBench/Program.cs
+++ iLynx.UI.TestBench/Program.cs
@@ -1,16 +1,46 @@
 ﻿//using iLynx.UI.OpenGL;
 
+using System;
+using iLynx.Common;
 using SFML.Window;
 using Window = iLynx.UI.Sfml.Window;
 
 namespace iLynx.UI.TestBench
 {
+    public class Foo
+    {
+        public string A
+        {
+            get;
+            set;
+        }
+    }
+
+    public class Bar
+    {
+        public string B
+        {
+            get;
+            set;
+        }
+    }
+
     public static class Program
     {
         static void Main(string[] args)
         {
+            var foo = new Foo();
+            var bar = new Bar();
+            var binding = new TwoWayBinding<string>(foo, "A");
+            binding.AddTarget(bar, "B");
+            Console.WriteLine($"A: {foo.A}");
+            //binding.SetValue("Something");
+            foo.A = "Something";
+            Console.WriteLine($"A: {foo.A}");
+            Console.WriteLine($"B: {bar.B}");
             var window = new Window(1280, 720, "Test");
             window.Show();
+
             //window.Closed += (s, e) => { window.Close(); };
             //while (window.IsOpen)
             //{
